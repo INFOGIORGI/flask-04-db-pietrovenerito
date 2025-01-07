552c5aff35dae085225e48f83db021f8292a8a4b
pietrovenerito <pietrovenerito28@gmail.com>
2025-01-07 09:52:09 +0000

    aggiunto

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"]="138.41.20.102"
+app.config["MYSQL_PORT"]="53306"
+app.config["MYSQL_DB"]="w3schools"
+app.config["MYSQL_USER"]="OSPITE"
+app.config["MYSQL_PASSWORD"]="OSPITE"
 
 @app.route("/")
 def home():
