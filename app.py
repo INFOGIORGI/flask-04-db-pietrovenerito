000c53e921a7fff08738e8d0c610f9af41125d9c
pietrovenerito <pietrovenerito28@gmail.com>
2025-01-09 16:56:08 +0000

    da provare

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"]="138.41.20.102"
-app.config["MYSQL_PORT"]="53306"
+app.config["MYSQL_PORT"]= 53306 #la porta è un intero
 app.config["MYSQL_DB"]="w3schools"
-app.config["MYSQL_USER"]="OSPITE"
-app.config["MYSQL_PASSWORD"]="OSPITE"
+app.config["MYSQL_USER"]="ospite"
+app.config["MYSQL_PASSWORD"]="ospite"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
@@ -13,6 +16,20 @@ def home():
 
 @app.route("/products")
 def products():
-    return render_template("products.html",titolo='Products')
+    cursor = mysql.connection.cursor() #CREAZIONE CURSORE
+    query="SELECT * FROM products"
+    cursor.execute(query)
+    prodotti=cursor.fetchall()
+    cursor.close()
+    return render_template("products.html",titolo='Products',prodotti=prodotti)
+
+@app.route("/categories/<categoryID>")
+def categories(categoryID):
+    cursor=mysql.connection.cursor()
+    query="SELECT * FROM categories WHERE CategoryID=%s"
+    cursor.execute(query,(categoryID,))
+    categorie=cursor.fetchall()
+    cursor.close()
+    return render_template("categories.html",titolo='categorie',categorie=categorie)
 
 app.run()
